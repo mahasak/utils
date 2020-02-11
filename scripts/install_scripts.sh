9303105ab9cba5b3ce39a305efb27b0f8860f368
Max Pijittum <mpijittum@fb.com>
2020-02-11 16:06:39 +0800

    Update script

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo ln -s ~/src/utils/script/fix_permission.sh /usr/local/bin/fix_permission
-#sudo ln -s ~/src/utils/script/start_postgres.sh /usr/local/bin/start_postgres
+sudo ln -s ~/src/utils/scripts/fix_permission.sh /usr/local/bin/fix_permission
+#sudo ln -s ~/src/utils/scripts/start_postgres.sh /usr/local/bin/start_postgres
 
